e53161cca4eebdc1781b70bdf98079052ecce240
knowmore <goooog@88.com>
2021-04-05 15:51:25 +0800

    update gh-pages

--- add-analyse.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i '/    <\/head>/ i <script type="text/javascript"> var _paq = window._paq = window._paq || []; _paq.push(["trackPageView"]); _paq.push(["enableLinkTracking"]); (function() { var u="//p.hotdry.top/matomo/"; _paq.push(["setTrackerUrl", u+"matomo.php"]); _paq.push(["setSiteId", "3"]); var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript"; g.async=true; g.src=u+"matomo.js"; s.parentNode.insertBefore(g,s); })(); </script>' ./**/*.html
-
-# <script type="text/javascript"> var _paq = window._paq = window._paq || []; _paq.push(["trackPageView"]); _paq.push(["enableLinkTracking"]); (function() { var u="//p.hotdry.top/matomo/"; _paq.push(["setTrackerUrl", u+"matomo.php"]); _paq.push(["setSiteId", "2"]); var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript"; g.async=true; g.src=u+"matomo.js"; s.parentNode.insertBefore(g,s); })(); </script>
